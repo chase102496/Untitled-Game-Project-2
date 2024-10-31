7a69435a017f68320abc2d1016a33c4a04639dd1
chase102496 <chase.h.parker@gmail.com>
2024-10-31 00:17:41 -0400

    Too much to list here
    Lots of level editor tweaks and aesthetics like ambient particles, waterfall particles fixed, rocks added, etc
    Also reorganized the directory so lots of weird things will happen for a bit before I fix them. Null pointers

---
+++ Logic/globals/glossary.gd
@@ -0,0 +1,12 @@
+extends Node
+
+const entity : Dictionary = {
+	"player" : preload("res://scenes/player.tscn"),
+	"enemy" : preload("res://scenes/enemy.tscn"),
+	"dreamkin" : preload("res://scenes/dreamkin.tscn")
+	}
+
+const particle : Dictionary = {
+	"fear" : preload("res://Art/particles/scenes/particle_fear.tscn"),
+	"burn" : preload("res://Art/particles/scenes/particle_burn.tscn")
+	}
